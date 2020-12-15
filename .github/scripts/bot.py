ce5a59a87c4b0715827e2ba4b5401543e14c920b
Adam Jeliński <ajelinski@antmicro.com>
2020-12-15 16:41:20 +0100

    Leave git and github mangling to the workflow

--- .github/scripts/bot.py
+++ .github/scripts/bot.py
@@ -2,60 +2,73 @@
 
 from os import environ, chdir
 from os.path import isdir, join, exists, dirname
-from subprocess import run, PIPE
+from subprocess import run, DEVNULL, PIPE
 from sys import exit
 from re import match, search, VERBOSE
 
-from github import Github
-
-def _run(cmd_string, return_stdout=False, **kwargs):
-    cmd = cmd_string.split()
-
+def _run(cmd_string, multiword_last_arg='', return_stdout=False, **kwargs):
+    cmd = cmd_string.split() + ([multiword_last_arg]
+            if multiword_last_arg else [])
     if return_stdout:
         return run(cmd, check=True, encoding='utf-8', stdout=PIPE,
                 **kwargs).stdout
     else:
         return run(cmd, check=True, **kwargs)
 
-def _get_env(env_name):
+def _get_env(env_name, required=True):
+    if env_name not in environ:
+        if required:
+            print('ERROR: Required environment variable not found: ' + env_name + '!')
+        return None
     env_var = environ[env_name]
     print('* ' + env_name + ': ' + env_var)
     return env_var
 
-conda_env = 'bot_env'
+def _remove_conda_env(conda_env_name):
+    print('Removing `' + conda_env_name + '` Conda environment... ', end='')
+    _run('conda env remove -n ' + conda_env_name, stdout=DEVNULL,
+            stderr=DEVNULL)
+    print('done!')
 
 print('Environment variables used are:')
-# The actual token will be replaced by asterisks in GH Actions log
-gh_token = _get_env('GITHUB_TOKEN')
-gh_repo_name = _get_env('GITHUB_REPOSITORY')
-
-branch_name = _get_env('BOT_BRANCH') + '_' + _get_env('GITHUB_RUN_ID')
+conda_env = _get_env('BOT_ENV_NAME')
 environment_path = _get_env('BOT_ENV_YML')
 conda_lock_path = _get_env('BOT_CONDA_LOCK')
-pip_lock_path = _get_env('BOT_PIP_LOCK')
-pr_base_branch_name = _get_env('BOT_PR_BASE')
+pip_lock_path = _get_env('BOT_PIP_LOCK', required=False)
 print()
+if None in [conda_env, environment_path, conda_lock_path]:
+    exit(1)
 
-
-_run('git config user.name BOT')
-_run('git config user.email <>')
-_run('git checkout -b ' + branch_name)
-
-_run('conda env remove -n ' + conda_env)
-_run('conda env create -n ' + conda_env + ' -f ' + environment_path)
+print('Creating `' + conda_env + '` environment based on `'
+        + environment_path + '`...')
+print()
+try:
+    _run('conda env create -n ' + conda_env + ' -f ' + environment_path)
+except:
+    print('ERROR: Creating `' + conda_env + '` environment failed!')
+    print('Please remove any environment with such name, if exists.')
+    print()
+    exit(1)
 
 # Capture explicit list of Conda packages
 conda_lock = _run('conda list --explicit -n ' + conda_env, return_stdout=True)
 
 # Capture frozen list of pip packages
 
-# It will stay None in case there's no pip in environment.yml
+# Variable will stay None in case there's no pip in environment.yml
 pip_lock = None
 
 # Check pip version
 pip_version_match = search(r'pip\-([0-9]+)\.([0-9]+)[^\-]+-[^\-]+\.conda',
         conda_lock, VERBOSE)
 if pip_version_match is not None:
+    if pip_lock_path is None:
+        print('ERROR: The environment uses pip dependencies but '
+                + 'BOT_PIP_LOCK environment variable hasn\'t been set!')
+        print()
+        _remove_conda_env(conda_env)
+        exit(1)
+
     major, minor = pip_version_match.groups()
     if int(major) < 20 or ( int(major) == 20 and int(minor) == 0 ):
         print('WARNING: The current version of pip (older than 20.1) is unable'
@@ -73,20 +86,24 @@ if pip_version_match is not None:
             print('Conda package removed from pip.lock: ' + conda_pkg_match.group(1))
     print()
 
+# Conda environment isn't needed anymore
+_remove_conda_env(conda_env)
+
 # Returns True if lock file has been updated, False otherwise
 def try_updating_lock_file(path, new_lock):
+    print('Trying to update `' + path + '`...')
     try:
         with open(path, 'r') as f:
             old_lock = f.read()
             if old_lock == new_lock:
-                print(path + ' is up to date')
+                print(path + ' is up to date.')
                 print()
                 return False
     except FileNotFoundError:
-        print(path + ' doesn\'t exist; it will be created')
-        print()
+        print(path + ' doesn\'t exist; it will be created.')
     with open(path, 'w') as f:
         f.write(new_lock)
+    print()
     return True
 
 updated_files = []
@@ -95,19 +112,12 @@ if try_updating_lock_file(conda_lock_path, conda_lock):
 if pip_lock and try_updating_lock_file(pip_lock_path, pip_lock):
     updated_files.append(pip_lock_path)
 
-# Commiting without any change will cause error
-if len(updated_files) > 0:
-    _run('git add ' + ' '.join(updated_files))
-    _run('git commit -m LOCK_BUMP ')
-    _run('git push -u origin ' + branch_name)
-
-    gh_repo = Github(gh_token).get_repo(gh_repo_name)
-    # It's necessary to pass 4 arguments so the body can't be skipped
-    gh_repo.create_pull(head=branch_name, base=pr_base_branch_name,
-            title='[BOT] Bump ' + ' and '.join(updated_files), body='')
-else:
+if len(updated_files) == 0:
     print('Both locks are up to date!')
+    exit(3)
+else:
+    print('Locks successfully updated:')
+    for file in updated_files:
+        print('* ' + file)
+    exit(0)
 
-# TODO remove?
-_run('git config --unset user.name')
-_run('git config --unset user.email')
